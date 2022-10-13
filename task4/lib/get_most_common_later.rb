501a85d872fbd6dd7600f8119fcb13d77e6c5184
Mykhailo Skliarenko <painliader@gmail.com>
2022-10-13 11:45:21 +0100

    refactor

--- task4/lib/get_most_common_later.rb
+++ task4/lib/get_most_common_later.rb
@@ -4,10 +4,10 @@ def get_most_common_letter(text)
   text.chars.each do |char|
     counter[char] += 1
   end
-  counter.to_a.max_by { |k, v| v }[0]
+  counter.to_a.max_by { |_k, v| v }[0]
 end
 
 # Intended output:
-# 
+#
 print get_most_common_letter("the roof, the roof, the roof is on fire!")
 # => "o"
